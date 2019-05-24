3bb89872777b5417058213fa6f9b34afa9c2f3dd
dajkam <sopotfilip@onet.pl>
2019-05-24 16:01:35 +0200

    działające zapytania crud

--- src/main/kotlin/com/filip/machaj/demo/service/ObywatelService.kt
+++ src/main/kotlin/com/filip/machaj/demo/service/ObywatelService.kt
@@ -2,17 +2,16 @@ package com.filip.machaj.demo.service
 
 import com.filip.machaj.demo.model.dane.Obywatel
 import com.filip.machaj.demo.repo.ObywatelRepo
-import com.filip.machaj.demo.repo.ObywatelRepoImpl
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import java.util.*
-import kotlin.random.Random
 
 
 @Service("Obywatel Service")
 class ObywatelService {
+    
+
     @Autowired
-    lateinit var repo : ObywatelRepoImpl
+    lateinit var repo : ObywatelRepo
 
 
 
