9b011660f7be875143f413989b958f80ad0d7cf2
dajkam <sopotfilip@onet.pl>
2019-06-04 01:37:54 +0200

    kolejna query

--- src/main/kotlin/com/filip/machaj/demo/repo/ObywatelRepo.kt
+++ src/main/kotlin/com/filip/machaj/demo/repo/ObywatelRepo.kt
@@ -7,4 +7,7 @@ import org.springframework.data.repository.CrudRepository
 interface ObywatelRepo : CrudRepository<Obywatel, Long>  {
     @Query("select * from obywatel where imie = ?1", nativeQuery = true)
     fun findByImie(imie:String): Iterable<Obywatel>
+
+    @Query("select * from obywatel where nazwisko like ?1", nativeQuery = true)
+    fun findByNazwisko(nazwisko:String): Iterable<Obywatel>
 }
